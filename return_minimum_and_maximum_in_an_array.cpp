3253511fa1506ddaf614289b29c8aa3bded64377
Nachiket16 <84851340+Nachiket16@users.noreply.github.com>
2021-12-28 13:53:56 +0530

    Add files via upload

---
+++ return_minimum_and_maximum_in_an_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void minmax(int *arr, int size){
+
+}
+
+
+int main(){
+int t;
+cin>>t;
+while (t--){
+    int n;
+    int *input= new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>i;
+    }
+    minmax(input, n);
+}
+return 0;
+}
